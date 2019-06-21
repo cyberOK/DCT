03dd12a7652c7013e2b9f030c6674ffa8eb2f2c7
Cyber <ntnkvart>
2019-06-21 12:40:38 +0300

    Rebild structure of project. Add Figures.BL dll

---
+++ Figures/Figures/FiguresForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Figures.Figure;
+
+namespace Figures
+{
+    public partial class FiguresForm : Form
+    {
+        #region Variables
+
+        private Graphics graphicsUnit;
+        private List<AbstractFigure> figuresOnDesk;
+        private Random randomForPosition;
+        private Point startPosition;
+        private float sizeForFigures = 50;
+        private Size sizeOfMainFiguresBox;
+
+        #endregion
+
+        #region Cunstructor of Form
+
+        public FiguresForm()
+        {
+            this.InitializeComponent();
+            this.figuresOnDesk = new List<AbstractFigure>();
+            this.timerForRedrawForm.Enabled = true;
+            this.sizeOfMainFiguresBox = this.MainFiguresBox.Size;           
+        }
+
+        #endregion
+
+        #region Events of form
+
+        private void timerForRedrawForm_Tick(object sender, EventArgs e)
+        {
+            using (graphicsUnit = MainFiguresBox.CreateGraphics())
+            {
+                MainFiguresBox.Refresh();
+                
+                foreach (AbstractFigure firstFigure in figuresOnDesk)
+                {
+                    if (figuresOnDesk.Count > 1)
+                    {
+                        CollisionCheck(firstFigure, figuresOnDesk);                       
+                    }
+
+                    firstFigure.Draw(graphicsUnit);
+                    firstFigure.Move((Point)sizeOfMainFiguresBox);
+                }                
+            }
+        }
+
+        private void MainFiguresBox_Resize(object sender, EventArgs e)
+        {
+            sizeOfMainFiguresBox = MainFiguresBox.Size;
+        }
+
+        private void buttonForTriangle_Click(object sender, EventArgs e)
+        {
+            startPosition = GetStartPositionCoordinates();
+            AbstractFigure triangle = new Triangle(startPosition, sizeForFigures);
+            figuresOnDesk.Add(triangle);
+            CreateNewTreeNode(triangle);
+
+            using (graphicsUnit = MainFiguresBox.CreateGraphics())
+            {
+                triangle.Draw(graphicsUnit);
+            }
+        }
+
+        private void buttonforCircle_Click(object sender, EventArgs e)
+        {
+            startPosition = GetStartPositionCoordinates();
+            AbstractFigure circle = new Circle(startPosition, sizeForFigures);
+            figuresOnDesk.Add(circle);
+            CreateNewTreeNode(circle);
+
+            using (graphicsUnit = MainFiguresBox.CreateGraphics())
+            {
+                circle.Draw(graphicsUnit);
+            }
+        }
+
+        private void buttonForRectangle_Click(object sender, EventArgs e)
+        {
+            startPosition = GetStartPositionCoordinates();
+            AbstractFigure rectangle = new Figure.Rectangle(startPosition, sizeForFigures);
+            figuresOnDesk.Add(rectangle);
+            CreateNewTreeNode(rectangle);
+
+            using (graphicsUnit = MainFiguresBox.CreateGraphics())
+            {
+                rectangle.Draw(graphicsUnit);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeFigures.SelectedNode;
+            
+            if (selectedNode != null)
+            {
+                var figure = (AbstractFigure)selectedNode.Tag;
+                if (figure.IsStoped)
+                    figure.IsStoped = false;
+                else figure.IsStoped = true;
+            }            
+        }
+
+        #endregion
+
+        #region Helpful methods
+
+        private void CreateNewTreeNode(AbstractFigure figureForNode)
+        {
+            TreeNode newNode = new TreeNode();
+            newNode.Text = figureForNode.ToString();
+            newNode.Tag = figureForNode;
+            treeFigures.Nodes.Add(newNode);
+        }
+
+        private Point GetStartPositionCoordinates()
+        {
+            randomForPosition = new Random();
+            Point startCoordinates = new Point
+            {
+                X = randomForPosition.Next((int)sizeForFigures, sizeOfMainFiguresBox.Width - (int)sizeForFigures),
+                Y = randomForPosition.Next((int)sizeForFigures, sizeOfMainFiguresBox.Height - (int)sizeForFigures)
+            };
+            return startCoordinates;
+        }
+
+        private void CollisionCheck(AbstractFigure figureToCheck, IEnumerable<AbstractFigure> figureCollection)
+        {
+            foreach (AbstractFigure secondFigure in figuresOnDesk)
+            {               
+                if (figureToCheck.Equals(secondFigure))
+                    continue;
+                else if (figureToCheck.IntersectZone.IntersectsWith(secondFigure.IntersectZone))
+                {
+                    figureToCheck.dx = -figureToCheck.dx;
+                    figureToCheck.dy = -figureToCheck.dy;
+                    figureToCheck.StartPosition = new Point { X = figureToCheck.dx, Y = figureToCheck.dy };
+
+                    secondFigure.dx = -figureToCheck.dx;
+                    secondFigure.dy = -figureToCheck.dy;
+                    secondFigure.StartPosition = new Point { X = secondFigure.dx, Y = secondFigure.dy };
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Figures/FiguresBase/Figure/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+namespace Figures.Figure
+{
+    public class Rectangle : AbstractFigure
+    {
+        public Rectangle(Point startPosition, float size) : base(startPosition, size)
+        { }
+
+        public override void Draw(Graphics graphicsUnit)
+        {
+            using (SolidBrush brushForFillFigure = new SolidBrush(Color.Red))
+            {
+                graphicsUnit.FillRectangle(brushForFillFigure, StartPosition.X, StartPosition.Y, this.SizeForFigure, this.SizeForFigure);
+            }
+        }
+
+        public override void Move(Point sizeOfMainPictureBox)
+        {
+            if (!this.IsStoped)
+            {
+                if (StartPosition.X <= 0 || StartPosition.X >= (sizeOfMainPictureBox.X - this.SizeForFigure))
+                {
+                    this.dx = -this.dx;
+                }
+
+                if (StartPosition.Y <= 0 || StartPosition.Y >= (sizeOfMainPictureBox.Y - this.SizeForFigure))
+                {
+                    this.dy = -this.dy;
+                }
+
+                this.StartPosition = new Point { X = dx, Y = dy };
+                this.IntersectZone = new System.Drawing.Rectangle { X = dx, Y = dy, Height = (int)SizeForFigure, Width = (int)SizeForFigure };
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Rectangle";
+        }
+    }
+}
